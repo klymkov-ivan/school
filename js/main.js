695fef82f6a0e0202211fadec8c1a525865bb7c9
klymkov_ivan <klymkov90@gmail.com>
2025-07-11 15:21:01 +0300

    First commit

---
+++ js/main.js
@@ -0,0 +1,47 @@
+(function($){
+	"use strict";
+	jQuery(document).on('ready', function () {
+        // Header Sticky
+		$(window).on('scroll',function() {
+            if ($(this).scrollTop() > 120){  
+                $('.navbar-area').addClass("is-sticky");
+            }
+            else{
+                $('.navbar-area').removeClass("is-sticky");
+            }
+        });
+		
+        // Button Hover JS
+        $(function() {
+            $('.default-btn')
+            .on('mouseenter', function(e) {
+                var parentOffset = $(this).offset(),
+                relX = e.pageX - parentOffset.left,
+                relY = e.pageY - parentOffset.top;
+                $(this).find('span').css({top:relY, left:relX})
+            })
+            .on('mouseout', function(e) {
+                var parentOffset = $(this).offset(),
+                relX = e.pageX - parentOffset.left,
+                relY = e.pageY - parentOffset.top;
+                $(this).find('span').css({top:relY, left:relX})
+            });
+        });
+
+        // Go to Top
+        $(function(){
+            // Scroll Event
+            $(window).on('scroll', function(){
+                var scrolled = $(window).scrollTop();
+                if (scrolled > 600) $('.go-top').addClass('active');
+                if (scrolled < 600) $('.go-top').removeClass('active');
+            });  
+            // Click Event
+            $('.go-top').on('click', function() {
+                $("html, body").animate({ scrollTop: "0" },  500);
+            });
+        });
+        
+    });
+
+}(jQuery));
